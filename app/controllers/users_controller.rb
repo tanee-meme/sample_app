ba34f9092f1deb3e15bf89976cc0233e1f92487a
tanee-meme <tanee.meme@gmail.com>
2020-11-06 20:09:23 +0900

    8.4 OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,18 +2,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # debugger
   end
 
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -22,9 +21,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
-  end
-
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
