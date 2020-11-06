162565566aae235e1dc23b752d5eff855b33689e
tanee-meme <tanee.meme@gmail.com>
2020-11-06 18:55:32 +0900

    Implement basic login 8.4

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'uglifier',     '3.2.0'
 gem 'coffee-rails', '4.2.2'
 gem 'jquery-rails', '4.3.1'
+gem 'jquery-ui-rails'
 
 gem 'rails-controller-testing'
 # Reduces boot times through caching; required in config/boot.rb
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
 const { environment } = require('@rails/webpacker')
+#2-7行目をコメントアウト
+// const { VueLoaderPlugin } = require('vue-loader')
+// const vue = require('./loaders/vue')
 
+// environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+// environment.loaders.prepend('vue', vue)
+// environment.config.resolve.alias = {'vue$': 'vue/dist/vue.esm.js' }
+#ここまで
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
 module.exports = environment
